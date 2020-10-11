cdfe148432be03d49b856b5bb20056ce3cb4b731
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2020-10-11 21:55:11 +0900

    Fix nanorc symlink in termux

--- etc/init/android/init.sh
+++ etc/init/android/init.sh
@@ -2,6 +2,6 @@
 
 root=$(dirname $0)
 
-ln -sfnv "$root/config/nano" "$DOTPATH/.config/nano"
+ln -sfnv "$root/.config/nano/nanorc" "$DOTPATH/.config/nano/nanorc"
 ln -sfnv "$root/.zprezto/init.zsh" "$DOTPATH/submodules/.zprezto/init.zsh"
 ln -sfnv "$root/.p10k.zsh" "$DOTPATH/.p10k.zsh"
