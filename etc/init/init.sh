9423a09785cfa4311caeb361a97f5bca7935ac3a
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2020-10-16 15:13:19 +0900

    Change platform initialize process

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -2,7 +2,9 @@
 
 source "$DOTPATH/etc/install"
 
-for i in "$DOTPATH/etc/init/$(platform)/init.sh"
+msg_header "platform: $(platform)"
+
+for i in "$DOTPATH"/etc/init/$(platform)/*.sh
 do
     if [ -f "$i" ]; then
         msg_arrow $(basename "$i")
