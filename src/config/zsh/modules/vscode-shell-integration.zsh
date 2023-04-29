dc3e3c5217e90561d34eb7041297ac9f80cb616e
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-04-29 10:05:49 +0900

    fix: git editor integration on vscode

--- src/config/zsh/modules/vscode-shell-integration.zsh
+++ src/config/zsh/modules/vscode-shell-integration.zsh
@@ -2,3 +2,8 @@
 
 ## Enable shell integration on romkatv/powerlevel10k.
 export POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
+
+## Set default editor on vscode integrated shell.
+export EDITOR="code -w"
+export GIT_EDITOR="code -w"
+export GIT_EXTERNAL_DIFF="code -w -d"
