7e6636fcd82bb36916c25a08316c29259f27d3ff
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-12-03 21:18:00 +0900

    feat: add ghq helpers in zsh

--- src/config/zsh/modules/fzf/setting.zsh
+++ src/config/zsh/modules/fzf/setting.zsh
@@ -7,3 +7,21 @@ export FZF_DEFAULT_OPTS="--layout=reverse --bind=ctrl-a:toggle-all,ctrl-space:to
 zstyle ":completion:*" format "[%d]"
 zstyle ":fzf-tab:*" switch-group "<" ">"
 zstyle ":fzf-tab:*" prefix ""
+
+## Functions
+### `cd` to `ghq` repositories directory on `zsh`.
+### Original: https://gist.github.com/sheepla/d680f1480d8c36c4290d6aabebf1abc6
+function _fzf_cd_ghq() {
+  local root="$(ghq root)"
+  local repo="$(
+    ghq list \
+    | fzf --preview="eza -1a --color=always --icons --group-directories-first --git ${root}/{1}"
+  )"
+  local target="${root}/${repo}"
+  [ -n "${target}" ] && cd "${target}"
+  zle accept-line
+  zle reset-prompt
+}
+
+zle -N  _fzf_cd_ghq
+bindkey "^[g" _fzf_cd_ghq
