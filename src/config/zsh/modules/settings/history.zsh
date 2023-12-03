111a7366229cde88e8d22f24d77dff41243ded05
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-12-03 13:16:34 +0900

    feat: add auto-remove failed command script on zsh

--- src/config/zsh/modules/settings/history.zsh
+++ src/config/zsh/modules/settings/history.zsh
@@ -3,7 +3,7 @@
 ## Environments
 HISTFILE="${XDG_STATE_HOME}/zsh/history"
 HISTSIZE=10000000
-SAVEHIST=10000000
+SAVEHIST=1000000
 
 ## Options
 setopt APPEND_HISTORY
@@ -22,3 +22,24 @@ setopt HIST_SAVE_NO_DUPS
 setopt HIST_VERIFY
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
+
+## Functions
+typeset -g _last_command=""
+function save_command() {
+  typeset -g _last_command="${1}"
+}
+
+function remove_failed_command_history() {
+  local _status="${?}"
+  local _successed_status=(0)
+  if [[ ${#_last_command} -gt 0 && ${_successed_status[(Ie)${_status}]} == 0 ]]; then
+    hist -s delete -1
+  fi
+  if [[ ${#_last_command} -gt 0 ]]; then
+    typeset -g _last_command=""
+  fi
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec save_command
+add-zsh-hook precmd remove_failed_command_history
