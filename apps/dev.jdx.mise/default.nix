7efb40fe16366edb40600d41804518e44dfccbe7
Ray Shirohara <RShirohara@proton.me>
2026-03-31 21:09:29 +0900

    chore(deps): update flake refs

--- apps/dev.jdx.mise/default.nix
+++ apps/dev.jdx.mise/default.nix
@@ -1,7 +1,5 @@
 { ... }: {
   programs.mise.enable = true;
-  # TODO: Build failing on 2026/03/30.
-  programs.mise.package = null;
 
   programs.mise.globalConfig = {
     settings.experimental = true;
