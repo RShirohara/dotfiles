f7adf49365dd063fa34c8f700c71edbbe7b6d3b2
Tatsuya Segawa <tatsuya.segawa@tss21.co.jp>
2026-06-25 14:40:21 +0900

    feat: add signing key to `tsegawa@pc-h0019054`

--- apps/com.git-scm.git/profiles/tatsuya.segawa_tss21.co.jp.nix
+++ apps/com.git-scm.git/profiles/tatsuya.segawa_tss21.co.jp.nix
@@ -3,15 +3,21 @@
     {
       condition = "gitdir:~/Workspace/github.com/**";
       contents = {
+        commit.gpgSign = true;
+        gpg.format = "ssh";
         user.email = "tatsuya.segawa@tss21.co.jp";
         user.name = "Tatsuya Segawa";
+        user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAMcgv6fs9sfysApnA2ssztYEjB6t24oRMmPtqPHrHLt";
       };
     }
     {
       condition = "gitdir:~/Workspace/gitlab.kccs-platform-dev.com/**";
       contents = {
+        commit.gpgSign = true;
+        gpg.format = "ssh";
         user.email = "tatsuya.segawa@tss21.co.jp";
         user.name = "Tatsuya Segawa";
+        user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAMcgv6fs9sfysApnA2ssztYEjB6t24oRMmPtqPHrHLt";
       };
     }
   ];
