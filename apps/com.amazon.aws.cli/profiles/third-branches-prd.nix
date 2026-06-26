a8e9eebf4a6213fa6a0f81301c6722750b8ab381
Ray Shirohara <RShirohara@proton.me>
2026-06-26 16:00:44 +0900

    fix: invalid profile config for `com.amazon.aws.cli`

--- apps/com.amazon.aws.cli/profiles/third-branches-prd.nix
+++ apps/com.amazon.aws.cli/profiles/third-branches-prd.nix
@@ -1,6 +1,7 @@
-{ ... }: {
+{ ... }:
+{
   programs.awscli.settings = {
-    "third-branches-prd" = {
+    "profile third-branches-prd" = {
       region = "ap-northeast-1";
       sso_account_id = "195790709853";
       sso_region = "ap-northeast-1";
