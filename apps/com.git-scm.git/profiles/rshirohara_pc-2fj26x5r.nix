222712f11682062db67b84066b2bf9904ab62b7d
Ray Shirohara <RShirohara@proton.me>
2026-07-27 09:37:28 +0900

    feat: change git commit signing key to ssh on `rshirohara@pc-2fj26x5r`

--- apps/com.git-scm.git/profiles/rshirohara_pc-2fj26x5r.nix
+++ apps/com.git-scm.git/profiles/rshirohara_pc-2fj26x5r.nix
@@ -4,18 +4,20 @@
       condition = "gitdir:~/Workspace/codeberg.org/**";
       contents = {
         commit.gpgSign = true;
+        gpg.format = "ssh";
         user.email = "RShirohara@proton.me";
         user.name = "Ray Shirohara";
-        user.signingKey = "E7321F26D8EACF39!";
+        user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKJNQ34Y8/tBKVVtw1KnkUgg9uE6VeqXJjqnjyTlTB0Y";
       };
     }
     {
       condition = "gitdir:~/Workspace/github.com/**";
       contents = {
         commit.gpgSign = true;
+        gpg.format = "ssh";
         user.email = "RShirohara@proton.me";
         user.name = "Ray Shirohara";
-        user.signingKey = "E7321F26D8EACF39!";
+        user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKJNQ34Y8/tBKVVtw1KnkUgg9uE6VeqXJjqnjyTlTB0Y";
       };
     }
   ];
